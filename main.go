ae5db4b08f3138c37669fe252e417b3e584f756c
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-11-26 12:04:31 +0900

    Support -bind argument

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+var bind = flag.String("bind", "0.0.0.0", "")
 var port = flag.String("port", "8080", "")
 var upstreamURL = flag.String("upstream-url", "http://localhost:9000", "")
 
@@ -16,7 +18,8 @@ func main() {
 		upstreamURL: *upstreamURL,
 	}
 
-	err := http.ListenAndServe(":"+*port, h)
+	addr := fmt.Sprintf("%s:%s", *bind, *port)
+	err := http.ListenAndServe(addr, h)
 	if err != nil {
 		log.Fatal(err)
 	}
