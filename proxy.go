b69a10000d1a0dca606a6740758cce347719aca4
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-09-12 08:10:57 +0900

    refactor: remove unnecessary description

--- proxy.go
+++ proxy.go
@@ -18,7 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// Package manael provides HTTP handler for processing images.
 package manael // import "manael.org/x/manael"
 
 import (
